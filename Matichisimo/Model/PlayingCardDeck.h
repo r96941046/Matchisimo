14940113bd9d2105f3d8a5411a48af4f3e27c422
Hung Yu-Ting <r96941046@gmail.com>
2014-06-12 00:26:41 +0800

    add class PlayingCardDeck

---
+++ Matichisimo/Model/PlayingCardDeck.h
@@ -0,0 +1,13 @@
+//
+//  PlayingCardDeck.h
+//  Matichisimo
+//
+//  Created by Hung Yu-Ting on 14/6/12.
+//  Copyright (c) 2014年 yt.hung. All rights reserved.
+//
+
+#import "Deck.h"
+
+@interface PlayingCardDeck : Deck
+
+@end
