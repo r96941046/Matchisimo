1430e157b06b75e063c3a69249fe49a109114ff5
Hung Yu-Ting <r96941046@gmail.com>
2014-06-14 11:34:42 +0800

    finished 12-card cardMatchGame

--- Matichisimo/CardGameViewController.m
+++ Matichisimo/CardGameViewController.m
@@ -8,21 +8,25 @@
 
 #import "CardGameViewController.h"
 #import "PlayingCardDeck.h"
+#import "CardMatchGame.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
 @property (strong, nonatomic) Deck *deck;
+@property (strong, nonatomic) CardMatchGame *game;
+@property (strong, nonatomic) IBOutletCollection(UIButton) NSArray *cardButtons;
+@property (weak, nonatomic) IBOutlet UILabel *scoreLabel;
 @end
 
 @implementation CardGameViewController
 
-- (Deck *)deck {
-    
-    if (!_deck) {
-        _deck = [self createDeck];
-    } return _deck;
+- (CardMatchGame *)game {
 
+    if (!_game) {
+        _game = [[CardMatchGame alloc]initWithCardCount:[self.cardButtons count] usingDeck:[self createDeck]];
+    }
+    return _game;
 }
 
 - (Deck *)createDeck {
@@ -39,21 +43,32 @@
 
 - (IBAction)touchCardButton:(UIButton *)sender {
     
-    if ([sender.currentTitle length]) {
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardBack"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"" forState:UIControlStateNormal];   
-    } else {
-        Card *randomCard = [self.deck drawRandomCard];
-        if (randomCard) {
-            [sender setBackgroundImage:[UIImage imageNamed:@"cardFront"]
-                              forState:UIControlStateNormal];
-            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
-        }
-      
-    }
-    self.flipCount++; // calls both getter and setter
+    NSUInteger chooseButtonIndex = [self.cardButtons indexOfObject:sender];
+    [self.game chooseCardAtIndex:chooseButtonIndex];
+    [self updateUI];
 }
 
+- (void)updateUI {
+
+    for (UIButton *cardButton in self.cardButtons) {
+        NSUInteger cardButtonIndex = [self.cardButtons indexOfObject:cardButton];
+        Card *card = [self.game cardAtIndex:cardButtonIndex];
+        [cardButton setTitle:[self titleForCard:card] forState:UIControlStateNormal];
+        [cardButton setBackgroundImage:[self backgroundImageForCard:card] forState:UIControlStateNormal];
+        cardButton.enabled = !card.isMatched;
+        self.scoreLabel.text = [NSString stringWithFormat:@"Score: %ld", (long) self.game.score];
+    }
+
+}
+
+- (NSString *)titleForCard:(Card *)card {
+
+    return card.isChosen ? card.contents : @"";
+}
+
+- (UIImage *)backgroundImageForCard:(Card *)card {
+
+    return [UIImage imageNamed:(card.isChosen ? @"cardFront" : @"cardBack")];
+}
 
 @end
--- Matichisimo/Model/CardMatchGame.m
+++ Matichisimo/Model/CardMatchGame.m
@@ -46,4 +46,39 @@
     return (index<[self.cards count]) ? self.cards[index] : nil;
 }
 
+static const int MATCH_PENALTY = 2;
+static const int MATCH_BONUS = 4;
+static const int COST_TO_CHOOSE = 1;
+
+- (void)chooseCardAtIndex:(NSUInteger)index {
+
+    Card *card = [self cardAtIndex:index];
+    
+    if (!card.isMatched) {
+        if (card.isChosen) {
+            card.chosen = NO;
+        } else {
+            
+            // match against another card
+            for (Card *otherCard in self.cards) {
+                if (otherCard.isChosen && !otherCard.isMatched) {
+                    int matchScore = [card match:@[otherCard]];
+                    if (matchScore) {
+                        self.score += matchScore * MATCH_BONUS;
+                        otherCard.matched = YES;
+                        card.matched = YES;
+                    } else {
+                        self.score -= MATCH_PENALTY;
+                        otherCard.chosen = NO;
+                    }
+                    break; // can only choose two cards for now
+                }
+            }
+            
+            self.score -= COST_TO_CHOOSE;
+            card.chosen = YES;
+        }
+    }
+}
+
 @end
